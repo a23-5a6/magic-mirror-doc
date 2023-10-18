e5cf9d1a6127698a4a0f89b43413c625ef3b2070
xalixilax <xalixilax@gmail.com>
2023-10-18 09:24:13 -0400

    feat: Added deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	base: '/magic-mirror-doc',
 	integrations: [
 		starlight({
 			title: 'My Docs',
